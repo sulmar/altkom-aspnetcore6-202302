74b0a518877576824103e3b100d344ec891149fd
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-02-06 10:47:16 +0100

    Utworzenie domeny

---
+++ src/Altkom.Shopper.Domain/Customer.cs
@@ -0,0 +1,23 @@
+﻿namespace Altkom.Shopper.Domain;
+
+public class Base
+{
+    
+}
+
+public class BaseEntity : Base
+{
+    public int Id { get; set; }
+}
+
+public class Customer : BaseEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
