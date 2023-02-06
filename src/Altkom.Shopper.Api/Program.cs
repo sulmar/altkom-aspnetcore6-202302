0bc38985964f521ed00365f4708d2733614edf51
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-02-06 12:01:49 +0100

    Działający DbContext

--- src/Altkom.Shopper.Api/Program.cs
+++ src/Altkom.Shopper.Api/Program.cs
@@ -1,11 +1,23 @@
 using Altkom.Shopper.Domain;
 using Altkom.Shopper.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddScoped<ICustomerRepository, DbCustomerRepository>();
+// builder.Services.AddScoped<ICustomerRepository, DbCustomerRepository>();
+
+string connectionString = "Data Source=shopper.db";
+// 1. dotnet add package Microsoft.EntityFrameworkCore.Sqlite
+builder.Services.AddDbContext<ShopperDb>(options => options.UseSqlite(connectionString));
+
+// 2. dotnet tool install --global dotnet-ef
+// 3. dotnet add package Microsoft.EntityFrameworkCore.Design --version 6.0.13
+// 4. dotnet ef migrations add Init
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/api/customers", (ICustomerRepository repository) => repository.GetAll());
+// app.MapGet("/api/customers", (ICustomerRepository repository) => repository.GetAll());
+
+app.MapGet("/api/customers", (ShopperDb db) => db.Customers.ToList());
+
 app.Run();
---
+++ src/Altkom.Shopper.Api/ShopperDb.cs
@@ -0,0 +1,14 @@
+﻿using Altkom.Shopper.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Altkom.Shopper.Infrastructure;
+
+// dotnet add package Microsoft.EntityFrameworkCore
+public class ShopperDb : DbContext
+{
+    public ShopperDb(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+}
