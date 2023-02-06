f9f3130dfea82a590c84a8bf1dd085c27c7b5aab
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-02-06 11:08:32 +0100

    Dodanie infrastruktury

---
+++ src/Altkom.Shopper.Infrastructure/DbCustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using Altkom.Shopper.Domain;
+
+namespace Altkom.Shopper.Infrastructure;
+
+public class DbCustomerRepository : ICustomerRepository
+{
+    public IEnumerable<Customer> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Customer GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
