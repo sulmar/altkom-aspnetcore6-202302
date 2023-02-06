3b65e9a88405022b7ccc17f665ce7197c6683849
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-02-06 12:14:33 +0100

    Utworzenie bazy danych

---
+++ src/Altkom.Shopper.Api/Extensions/ServicesExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Altkom.Shopper.Api.Extensions;
+
+public static class ServicesExtensions
+{
+    public static async Task EnsureDatabase<TContext>(this IServiceProvider services)        
+        where TContext : DbContext
+    {
+        await using var db = services.CreateScope().ServiceProvider.GetRequiredService<TContext>();
+
+        await db.Database.MigrateAsync();
+
+    }
+}
--- src/Altkom.Shopper.Api/Program.cs
+++ src/Altkom.Shopper.Api/Program.cs
@@ -1,3 +1,4 @@
+using Altkom.Shopper.Api.Extensions;
 using Altkom.Shopper.Domain;
 using Altkom.Shopper.Infrastructure;
 using Microsoft.EntityFrameworkCore;
@@ -9,11 +10,15 @@ string connectionString = "Data Source=shopper.db";
 // 1. dotnet add package Microsoft.EntityFrameworkCore.Sqlite
 builder.Services.AddDbContext<ShopperDb>(options => options.UseSqlite(connectionString));
 
+
+
 // 2. dotnet tool install --global dotnet-ef
 // 3. dotnet add package Microsoft.EntityFrameworkCore.Design --version 6.0.13
 // 4. dotnet ef migrations add Init
 
 var app = builder.Build();
+
+await app.Services.EnsureDatabase<ShopperDb>();
 
 app.MapGet("/", () => "Hello World!");
 // app.MapGet("/api/customers", (ICustomerRepository repository) => repository.GetAll());
